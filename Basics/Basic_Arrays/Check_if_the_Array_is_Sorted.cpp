a7c9299b206bdd8e5daa23958b7f91eca7f61e1d
jayamuralikrishnaperuru <muralikrishna3407@gmail.com>
2026-03-13 15:04:08 +0530

    added the file to count the frequency of the elements

--- Basics/Basic_Arrays/Check_if_the_Array_is_Sorted.cpp
+++ Basics/Basic_Arrays/Check_if_the_Array_is_Sorted.cpp
@@ -1,26 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool arraySortedOrNot(int arr[], int n) {
-        for(int i = 0; i < n-1; i++){
-            if(arr[i] > arr[i+1])return false ;
+    bool arraySortedOrNot(int arr[], int n)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+                return false;
         }
-        return true ;
+        return true;
     }
 };
 
-
-
-int main() {
-    Solution solution;    
+int main()
+{
+    Solution solution;
     int n = 5;
     int arr[] = {1, 2, 3, 4, 5};
     bool sorted = solution.arraySortedOrNot(arr, n);
-    if (sorted) {
+    if (sorted)
+    {
         cout << "Array is sorted." << endl;
-    } else {
+    }
+    else
+    {
         cout << "Array is not sorted." << endl;
     }
     return 0;
---
+++ Basics/Basic_Arrays/Count Frequency of elements.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == arr[i + 1])
+        {
+            cout << arr[i] << " ";
+        }
+    }
+    return 0;
+}
