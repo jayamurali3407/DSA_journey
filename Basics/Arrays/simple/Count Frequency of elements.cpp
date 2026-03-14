9cbd167bbcfe5ac86ff03d432a88010bd041c3ac
jayamuralikrishnaperuru <muralikrishna3407@gmail.com>
2026-03-14 10:10:01 +0530

    Count the frequency of every element using mapping

--- Basics/Arrays/simple/Count Frequency of elements.cpp
+++ Basics/Arrays/simple/Count Frequency of elements.cpp
@@ -1,3 +1,27 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     vector<int> arr(n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     int count = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] == arr[i + 1])
+//         {
+//             cout << arr[i] << " ";
+//         }
+//         count += 1 ;
+//     }
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -5,17 +29,19 @@ int main()
 {
     int n;
     cin >> n;
+
     vector<int> arr(n);
+    unordered_map<int, int> freq;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
+        freq[arr[i]]++; // increase count
     }
-    for (int i = 0; i < n; i++)
+
+    for (auto x : freq)
     {
-        if (arr[i] == arr[i + 1])
-        {
-            cout << arr[i] << " ";
-        }
+        cout << x.first << " occurs " << x.second << " times\n";
     }
+
     return 0;
 }
