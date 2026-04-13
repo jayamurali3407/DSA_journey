e4877e49e5e7e103eaf1835dc86f6eca5ab7717b
jayamuralikrishnaperuru <muralikrishna3407@gmail.com>
2026-04-13 16:41:11 +0530

    Finds the second largest distinct element in an array by sorting it and scanning from the end.

--- Basics/Arrays/simple/Second largest element.cpp
+++ Basics/Arrays/simple/Second largest element.cpp
@@ -1,16 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    int secondlargestelement(vector<int> &nums)
+    {
+        int k = nums.size();
+        int largest = nums.back();
+        int secondlargest = -1;
+
+        for (int i = k - 2; i >= 0; i--)
+        {
+            if (nums[i] != largest)
+            {
+                secondlargest = nums[i];
+                break;
+            }
+        }
+        return secondlargest;
+    }
+};
+
 int main()
 {
+    Solution sol;
     int n;
+    cout << "Enter the size of the element : " << endl;
     cin >> n;
     vector<int> arr(n);
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    sort(arr.begin(), arr.end());
-    cout << "Second largest in an array is : " << arr[n - 2];
+
+    cout << "The second largest element is : " << sol.secondlargestelement(arr) << endl;
     return 0;
 }
